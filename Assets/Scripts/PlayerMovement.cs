524296ac58ae5e05242bb18e46af06108319c1ae
MarkBebrov <pro100nemo666@gmail.com>
2023-10-29 12:02:49 +0300

    анимция движения

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,19 +9,27 @@ public class PlayerMovement : NetworkBehaviour
     [SerializeField] private float walkSpeed = 5f;
     [SerializeField] private float runSpeed = 10f;
 
+    private const float DIRECTION_THRESHOLD = 0.1f;
+
     private Rigidbody2D rb;
     private Vector2 movement;
     private NetworkIdentity networkIdentity;
 
-    private bool isDirectionalMovement = false; // Переменная для проверки режима движения
-    [SerializeField] private float backwardSpeed = 2.5f; // Скорость движения назад
-    [SerializeField] private float sideStepSpeed = 3f; // Скорость движения вбок
+    private bool isDirectionalMovement = false;
+    [SerializeField] private float backwardSpeed = 2.5f;
+    [SerializeField] private float sideStepSpeed = 3f;
     [SerializeField] private bool isRunning = false;
+
+    private Animator animator;
+
+    public enum Direction { Up, Down, Left, Right }
+    private Direction lastDirection = Direction.Down;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         networkIdentity = GetComponent<NetworkIdentity>();
+        animator = GetComponent<Animator>();
     }
 
     public Vector2 GetCurrentMovement()
@@ -35,14 +43,14 @@ public class PlayerMovement : NetworkBehaviour
         {
             return;
         }
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
             isRunning = true;
-        else if(Input.GetKeyUp(KeyCode.LeftShift))
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
             isRunning = false;
+
         if (Input.GetKeyDown(KeyCode.LeftAlt))
-        {
-            isDirectionalMovement = !isDirectionalMovement; // Переключение режима движения
-        }
+            isDirectionalMovement = !isDirectionalMovement;
 
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
@@ -53,16 +61,12 @@ public class PlayerMovement : NetworkBehaviour
             directionToMouse.Normalize();
 
             Vector2 forwardMovement = Vector2.zero;
-            if (moveY > 0) // Движение вперед
-            {
+            if (moveY > 0)
                 forwardMovement = moveY * directionToMouse;
-            }
-            else if (moveY < 0) // Движение назад
-            {
+            else if (moveY < 0)
                 forwardMovement = moveY * backwardSpeed * directionToMouse;
-            }
 
-            Vector2 perpendicular = new Vector2(directionToMouse.y, -directionToMouse.x); // Инвертированное перпендикулярное направление
+            Vector2 perpendicular = new Vector2(directionToMouse.y, -directionToMouse.x);
             Vector2 sideMovement = moveX * sideStepSpeed * perpendicular;
 
             movement = (forwardMovement + sideMovement).normalized;
@@ -73,14 +77,38 @@ public class PlayerMovement : NetworkBehaviour
             movement = new Vector2(moveX, moveY).normalized;
             movement *= isRunning ? runSpeed : walkSpeed;
         }
+
+        if (Mathf.Abs(movement.x) > DIRECTION_THRESHOLD || Mathf.Abs(movement.y) > DIRECTION_THRESHOLD)
+        {
+            if (movement.x > DIRECTION_THRESHOLD)
+            {
+                lastDirection = Direction.Right;
+            }
+            else if (movement.x < -DIRECTION_THRESHOLD)
+            {
+                lastDirection = Direction.Left;
+            }
+            else if (movement.y > DIRECTION_THRESHOLD)
+            {
+                lastDirection = Direction.Up;
+            }
+            else if (movement.y < -DIRECTION_THRESHOLD)
+            {
+                lastDirection = Direction.Down;
+            }
+        }
+
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetBool("LastDirectionRight", lastDirection == Direction.Right);
+        animator.SetBool("LastDirectionUp", lastDirection == Direction.Up);
     }
 
     private void FixedUpdate()
     {
         if (!networkIdentity.isOwned)
-        {
             return;
-        }
 
         MoveCharacter();
     }
